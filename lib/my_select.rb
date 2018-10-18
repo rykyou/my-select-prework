12095692f99d3e09609f0fee3912fdae3cd8551c
Robin Kim <rkim1214@gmail.com>
2018-10-18 13:37:04 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,15 @@
 def my_select(collection)
- # your code here!
+  i = 0 
+  new_collection = []
+  
+  while i < collection.length 
+    yield collection[i]
+    if collection[i] == true  
+      true_element = collection[i]
+      new_collection << true_element
+      i += 1 
+    else 
+      i += 1 
+    end 
+  end 
 end
